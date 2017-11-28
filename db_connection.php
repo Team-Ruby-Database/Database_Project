ba6e0d2474f3afd4f49ea26079d830776fb3266a
Yin Song <31377159+songyin79@users.noreply.github.com>
2017-11-28 16:45:32 -0500

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,31 @@
+<!--Team Ruby--
+This file builds a connection to the database HacherTracker.
+To use it, put this line on the top of your code:
+include ‘connection.php’;
+-->
+
+<?php
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "centre79,";
+ $db = "HackerTracker";
+ 
+ 
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+ /* or die("Connect failed: %s\n". $conn -> error); */
+ 
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
+?>

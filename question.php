3ff4ac9d2992754cbb6577271a87cf9f224b4598
Yin Song <31377159+songyin79@users.noreply.github.com>
2017-12-01 15:14:57 -0500

    Update question.php

--- question.php
+++ question.php
@@ -2,24 +2,11 @@
 --Victoria Cummings,Yin Song, Michelle Kim, Xiao Jiang
 --Shows all the questions related to the skill that was clicked on the previous page.
 --!>
+
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<head>
-<style>
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-td,th {
-	padding: 15px;
-	
-    text-align: left;
-    padding: 8px;
-}
-</style>
-</head>
+
 <body>
 
 <?php
@@ -30,14 +17,18 @@ td,th {
 include 'db_connection.php';
 $conn = OpenCon();
 session_start();
+
+//get course_id, student_id by session
+$Course_ID= $_SESSION['Course_ID'];
+$Student_ID=$_SESSION['Student_ID'];
+
+//get skill id by get
 $Skill_ID= $_GET['Skill_ID'];
-//$Skill_ID=1;
-//$Student_ID= $_GET['Student_ID'];
-$Student_ID=3;
-$Course_ID=$_SESSION['Course_ID'];
-$_SESSION['Student_ID']=$Student_ID;
+
+//build session for skill_id
 $_SESSION['Skill_ID']=$Skill_ID;
-$_SESSION['Course_ID']=$Course_ID;
+
+// present skill name
 $sql="SELECT Skill_Name FROM Skills WHERE Skills.Skill_ID=$Skill_ID";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
@@ -49,6 +40,9 @@ if ($result->num_rows > 0) {
 } else {
     echo "NO such skill";
 }
+
+//display all the questions of the skill
+//each question has an input box
 $sql="SELECT Question,Question_ID FROM Questions WHERE Questions.Skill_ID=$Skill_ID";
 $result = $conn->query($sql);
 echo "<ol><form action='feedback.php?Skill_ID=".$Skill_ID."'"."method='POST'>";
@@ -56,20 +50,18 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
     	echo "<li>".'<div class="w3-padding w3-light-grey"><p class="w3-large" style="margin-bottom:30px;">';
         echo $row["Question"]."</p>";
-        echo "Answer: <input type='text' name=".'q'.$row['Question_ID']."><br><br>";
+        echo "Answer: <input type='text' name=".'q'.$row['Question_ID']." required><br><br>";
         echo '</li></div><br>';
     }
     
-    echo "<hr><font color='yellow'><div class='w3-container'>";
-	echo "<input class='w3-btn w3-black w3-text-yellow' type='submit' value='Submit'></font></form></ol></div>";
+    echo "<hr><font color='yellow'><div class='w3-container' align='center' >";
+	echo "<input class='w3-btn w3-black w3-text-yellow' type='submit' value='Submit' ></font></form></ol></div>";
     
 } else {
     echo "<p align='center'>NO practice problem available</p> <br><br>";
-    echo "</table><font color='yellow'><div align='center' class='w3-container'>";
-	echo "<input class='w3-btn w3-black w3-text-yellow' type='submit' formaction='/studentclasses.php' value='Go back to Skill List'></font></form></ol></div>";
 }
 CloseCon($conn);
 ?>
 
 </body>
-	</html>
+</html>

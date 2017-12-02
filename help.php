3dab65093c4df006fb30a87be2ecc1feecdb747a
cazes67 <33919714+cazes67@users.noreply.github.com>
2017-12-01 21:51:40 -0500

    Add files via upload

---
+++ help.php
@@ -0,0 +1,30 @@
+<?php
+/*Team Ruby*/
+/*
+Victoria Cummings
+Michelle Kim
+Yin Song
+Xiao Jiang
+
+help  page
+*/
+?>
+<html>
+<body>
+
+    <h1>Help:</h1>
+    <p1>
+        Help infomation is waiting to be added...
+    </p1>
+    <br>
+    <br>
+    <?php $back = $_POST['pageName']."php";
+
+    ?>
+
+    <br>
+
+    <a href=back>Back to the last page-></a>
+
+</body>
+</html>

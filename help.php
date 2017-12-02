de155339e5700a962eb47978357f72bb3a47b962
cazes67 <33919714+cazes67@users.noreply.github.com>
2017-12-01 23:33:40 -0500

    Update help.php

--- help.php
+++ help.php
@@ -1,30 +1,28 @@
-<?php
-/*Team Ruby*/
-/*
-Victoria Cummings
-Michelle Kim
-Yin Song
-Xiao Jiang
-
-help  page
-*/
-?>
 <html>
 <body>
 
-    <h1>Help:</h1>
-    <p1>
-        Help infomation is waiting to be added...
-    </p1>
-    <br>
-    <br>
-    <?php $back = $_POST['pageName']."php";
+<h1>Help:</h1>
+<p1>
+    Help intructions:
+</p1>
+<br>
+<br>
+<?php $lastpage = $_POST['value']."php";
+if($lastpage=='main_login')//page name
+{
+    echo "Please input your email and password correctly, and remember to choose the class you belong to (students or professors)";
+}
+else if($lastpage=="signup")
+{
+    echo "Please input your information correctly and make sure choose the right class you belong to (students or professors)";
+}
 
-    ?>
 
-    <br>
+?>
 
-    <a href=back>Back to the last page-></a>
+<br>
+
+<a href=$lastpage>Back to the last page-></a>
 
 </body>
 </html>
